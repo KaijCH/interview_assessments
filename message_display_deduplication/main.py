8cc6ffeda035f5b91adfc2069bc6ad84ee3f064a
kaijCH <kaijchen21@gmail.com>
2025-06-03 15:26:03 -0700

    chore: adding more tests;

--- message_display_deduplication/main.py
+++ message_display_deduplication/main.py
@@ -1,6 +1,19 @@
 from message_deduplication import TestBed, deduplicates
 
 def main() -> None:
+    # empty message
+    msgs = []
+    test1 = TestBed(tests=msgs,  threshold=15)
+    assert deduplicates(test1) == []
+    
+    # signle message
+    msgs = [
+        [0, "Low Power"]
+    ]
+    test1 = TestBed(tests=msgs,  threshold=10)
+    assert deduplicates(test1) == [[0, "Low Power"]]
+
+    # multiple overlap message
     msgs = [
         [0, "Low Power"],
         [1, "High Temperature"],
@@ -10,14 +23,8 @@ def main() -> None:
         [13, "Out of Memory"],
         [14, "High Temperature"],
     ]
-    test1 = TestBed(msgs, 10)
+    test1 = TestBed(tests=msgs, threshold=10)
     assert deduplicates(test1) == [[1, "High Temperature"],[12, "Low Power"],[13, "Out of Memory"],[14, "High Temperature"]]
-
-    msgs = [
-        [0, "Low Power"]
-    ]
-    test1 = TestBed(msgs, 10)
-    assert deduplicates(test1) == [[0, "Low Power"]]
 
     print("overall success")
 
