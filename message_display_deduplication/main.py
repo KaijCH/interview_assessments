69f8fbfec18d42a35e5b6535deb35843d26a682a
kaijCH <kaijchen21@gmail.com>
2025-06-03 21:53:29 -0700

    fix: reordering testcases;

--- message_display_deduplication/main.py
+++ message_display_deduplication/main.py
@@ -3,15 +3,17 @@ from message_deduplication import TestBed, deduplicates
 def main() -> None:
     # empty message
     msgs = []
-    test1 = TestBed(tests=msgs,  threshold=15)
-    assert deduplicates(test1) == []
+    test0 = TestBed(tests=msgs,  threshold=3)
+    expt0 = []
+    assert deduplicates(test0) == expt0, "failure in handling none message"
     
     # signle message
     msgs = [
         [0, "Low Power"]
     ]
-    test1 = TestBed(tests=msgs,  threshold=10)
-    assert deduplicates(test1) == [[0, "Low Power"]]
+    test1 = TestBed(tests=msgs,  threshold=1)
+    expt1 = [[0, "Low Power"]]
+    assert deduplicates(test1) == expt1, "failure in rendering single message"
 
     # multiple overlap message
     msgs = [
@@ -23,8 +25,9 @@ def main() -> None:
         [13, "Out of Memory"],
         [14, "High Temperature"],
     ]
-    test1 = TestBed(tests=msgs, threshold=10)
-    assert deduplicates(test1) == [[1, "High Temperature"],[12, "Low Power"],[13, "Out of Memory"],[14, "High Temperature"]]
+    test2 = TestBed(tests=msgs, threshold=10)
+    expt2 = [[1, "High Temperature"],[12, "Low Power"],[13, "Out of Memory"],[14, "High Temperature"]]
+    assert deduplicates(test2) == expt2, "failure in filtering duplication"
 
     print("overall success")
 
