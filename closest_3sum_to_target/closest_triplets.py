653300697891ebbc7d444d0c2cc7c9b420e89141
kaijCH <kaijchen21@gmail.com>
2025-06-06 13:19:09 -0700

    fix: implementing validates() for res;

--- closest_3sum_to_target/closest_triplets.py
+++ closest_3sum_to_target/closest_triplets.py
@@ -1,21 +1,39 @@
 import math
 
+class Triplet:
+    
+    def __init__(self, left: int, midd: int, rite: int) -> None:
+        self.triplet = [left, midd, rite]
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.triplet))
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Triplet): return False
+        return self.__hash__() == other.__hash__()
+
+    def sums(self) -> int:
+        return sum(self.triplet)
+
 
 def finds_triplets(nums: list, target: int = 0) -> list[list[int]]:
     closest, length = math.inf, len(nums)
     candidates = list()
+    visits = set()
     nums.sort()
     for left in range(length - 2):
         midd, rite = left + 1, length - 1
         while midd < rite:
-            current = [nums[left] , nums[midd] , nums[rite]]
-            sumup = sum(current)
+            current = Triplet(nums[left] , nums[midd] , nums[rite])
+            sumup = current.sums()
             distance = abs(target - sumup)
-            if distance < closest:
-                closest = distance
-                candidates = [current]
-            if distance == closest and current not in candidates:
+            if current not in visits:
+                if distance < closest:
+                    closest = distance
+                    candidates = [current]
+                if distance == closest and current not in candidates:
                     candidates.append(current)
+                visits.add(current)
             if sumup <= target:
                 tempa = nums[midd]
                 while midd < rite and nums[midd] == tempa:
@@ -24,5 +42,5 @@ def finds_triplets(nums: list, target: int = 0) -> list[list[int]]:
                 tempa = nums[rite]
                 while midd < rite and nums[rite] == tempa:
                     rite -= 1
-                
-    return candidates
+                    
+    return [item.triplet for item in candidates]
--- closest_3sum_to_target/main.py
+++ closest_3sum_to_target/main.py
@@ -1,14 +1,15 @@
 from closest_triplets import finds_triplets
 
+
 def validates(exp: list, res: list) -> bool:
-    return True
+    return sorted(sorted(e for e in exp)) == sorted(sorted(r for r in res))
 
 
 def main() -> None:
     nums1, target1 = [1, 2, 3], 6
     expt1 = [[1, 2, 3]]
     res1 = finds_triplets(nums1, target1)
-    assert validates(expt1, res1) == True, "failure in handing 3 elements"
+    assert validates(expt1, res1) == True, "failure in handing 3 input elements"
 
     nums2, target2 = [1, 2, 3], 20
     expt2 = [[1, 2, 3]]
@@ -18,13 +19,13 @@ def main() -> None:
     nums3, target3 = [9, -1, 1, 1, 2, 3, 3, 20, 19, 11], -1
     expt3 = [[-1, 1, 1]]
     res3 = finds_triplets(nums3, target3)
-    assert validates(expt3, res3) == True, "failure in handing closest match"
+    assert validates(expt3, res3) == True, "failure in handing closest match with negative elements"
 
     nums4, target4 = [1, 2, 3, -1, 8, -1, 19], 6
     expt4 = [[1, 2, 3], [-1, -1, 8]]
     res4 = finds_triplets(nums4, target4)
     
-    assert validates(expt4, res4) == True, "failure in handing 3 elements"
+    assert validates(expt4, res4) == True, "failure in handing multi-results"
     
 
     print("overall success")
