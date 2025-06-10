dd68e97691d50d4d2aa31efef206c3ff30fa8e95
kaijCH <kaijchen21@gmail.com>
2025-06-10 03:17:24 -0700

    fix: deleting redundant ref logic;

--- faltten_elements_from_list/flatten_list.py
+++ faltten_elements_from_list/flatten_list.py
@@ -2,18 +2,22 @@ import collections
 
 # iterative implementation
 def flattens_iterative(elements: list) ->list:
-    if not elements: return []
+    if not elements: 
+        return []
     opts = collections.deque(elements)
     visits, res = {id(elements)}, []
     while opts:
         curr = opts.popleft()
-        if not curr: continue
+        if not curr: 
+            continue
         if isinstance(curr, int):
             res.append(curr)
             continue
-        if id(curr) in visits: continue
-        visits.add(id(curr))
-        while curr:
+        memo = id(curr)
+        if memo in visits: 
+            continue
+        visits.add(memo)
+        while curr: 
             ele = curr.pop()
             opts.appendleft(ele)
     return res
@@ -21,19 +25,19 @@ def flattens_iterative(elements: list) ->list:
 
 # recursive implementation
 def recurs(elements: list, visits: set) -> list:
-    res = []
     visits.add(id(elements))
+    res = []
     for ele in elements:
-        if id(ele) in visits:
-            continue
         if isinstance(ele, int):
             res.append(ele)
-        else:
-            res += recurs(ele, visits)
+            continue
+        if id(ele) in visits:
+            continue
+        res += recurs(ele, visits)
     return res
 
 def flattens_recursive(elements: list) -> list:
-    if not elements: return []
+    if not elements: 
+        return []
     visits = set()
-    visits.add(id(elements))
     return recurs(elements,  visits)
